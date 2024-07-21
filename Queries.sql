04cff8367a72f88c69c84f6dbe25dad493f94ca1
Gustavo Cassimiro <142855595+Cassimirogustavo@users.noreply.github.com>
2024-07-21 17:51:38 +0000

    commit T

--- Queries.sql
+++ Queries.sql
@@ -1,9 +1,17 @@
--- database: ecommerce.db
+-- Selecionar todos os dados da tabela vendas
 SELECT * FROM vendas;
 
+
+-- Selecionar clientes que pagaram mais de 1000 reais
+SELECT * FROM vendas WHERE valor_pago > 1000;
+
+-- Selecionar vendas de um produto específico
+SELECT * FROM vendas WHERE produto = 'Brinquedo';
+
+-- Selecionar vendas por ano
 SELECT strftime('%Y', data_pagamento) AS ano, COUNT(*) AS total_vendas
 FROM vendas
 GROUP BY ano;
 
+-- Selecionar vendas com status de entrega específico
 SELECT * FROM vendas WHERE status_entrega = 'Entregue';
-
