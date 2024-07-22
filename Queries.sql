bbf95fb9bf3a7c1f81277d2c591d5a478528350f
Gustavo Cassimiro <142855595+Cassimirogustavo@users.noreply.github.com>
2024-07-22 17:03:13 +0000

    c5

--- Queries.sql
+++ Queries.sql
@@ -4,15 +4,23 @@ SELECT * FROM vendas;
 
 
 -- Selecionar clientes que pagaram mais de 1000 reais
-SELECT * FROM vendas WHERE valor_pago > 1000;
+SELECT * FROM vendas 
+WHERE valor_pago > 1000;
 
 -- Selecionar vendas de um produto específico
-SELECT * FROM vendas WHERE produto = 'Brinquedo';
+SELECT * FROM vendas 
+WHERE produto = 'Brinquedo';
 
 -- Selecionar vendas por ano
 SELECT strftime('%Y', data_pagamento) AS ano, COUNT(*) AS total_vendas
 FROM vendas
 GROUP BY ano;
 
+
 -- Selecionar vendas com status de entrega específico
-SELECT * FROM vendas WHERE status_entrega = 'Entregue';
+SELECT * FROM vendas 
+WHERE status_entrega = 'Entregue';
+
+-- Selecionar vendas com produtos iguais a ferramenta
+SELECT * FROM vendas 
+WHERE produto = 'Ferramenta';
