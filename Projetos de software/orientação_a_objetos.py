67463fcf54604f3c6ccd079ca7f73e8f45e82fba
Gustavo Cassimiro <142855595+Cassimirogustavo@users.noreply.github.com>
2024-08-15 02:20:49 +0000

    22

---
+++ Projetos de software/orientação_a_objetos.py
@@ -0,0 +1,14 @@
+nome = 'Tux'
+score = 8888
+
+nome2 = 'Hckr'
+score2 = 9898
+
+nome4 = 'Gamer'
+score4 = 9000
+
+print(f'O jogador {nome} marcou {score} pontos')
+print(f'O Jogador {nome2} marcou {score2} pontos')
+print(f'O Jogador {nome4} marcou {score4} pontos')
+
+jogador1 = 
